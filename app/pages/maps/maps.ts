f92fc1de2c1aeefbb3319efe976040c353ce0e35
Joy C. Toledo <thejoytoledo@gmail.com>
2016-09-17 21:39:40 +0800

    Initial Commit

---
+++ app/pages/maps/maps.ts
@@ -0,0 +1,44 @@
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
+
+declare var google;
+
+@Component({
+  templateUrl: 'build/pages/maps/maps.html'
+})
+
+export class Maps {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidEnter(){
+    this.loadMap();
+  }
+
+  loadMap(){
+    
+    Geolocation.getCurrentPosition().then((position) => {
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }, (err) => {
+      console.log(err);
+    });
+
+  }
+
+}
