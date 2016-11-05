0e979b74b596d193efde0e2d93189b99cb4db3e6
thejoytoledo <thejoytoledo@gmail.com>
2016-11-06 00:55:06 +0800

    added feature of marker just to test its functionality to the map

--- app/pages/maps/maps.ts
+++ app/pages/maps/maps.ts
@@ -22,7 +22,7 @@ export class Maps {
   }
 
   loadMap(){
-    
+
     Geolocation.getCurrentPosition().then((position) => {
 
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -35,10 +35,37 @@ export class Maps {
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-    }, (err) => {
-      console.log(err);
+    }, (success) => {
+    console.log(success);
     });
 
   }
+
+addMarker(){
+
+  let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: this.map.getCenter()
+  });
+
+  let content = "<h4>Information!</h4>";
+
+  this.addInfoWindow(marker, content);
+
+}
+
+addInfoWindow(marker, content){
+
+  let infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+
+  google.maps.event.addListener(marker, 'click', () => {
+    infoWindow.open(this.map, marker);
+  });
+
+}
+
 
 }
